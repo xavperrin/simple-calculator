5812d7c0d7d4eff63575f74476487316056487cd
xavperrin <xl.perrin@gmail.com>
2022-12-28 14:27:59 +0100

    add toBeFalsy & toBeTruthy spec

--- calculator-spec.js
+++ calculator-spec.js
@@ -1,6 +1,6 @@
-xdescribe("calculator.js", ()=>{
+describe("calculator.js", ()=>{
     //TODOS: specs
-    xit('should add numbers to total', function(){
+    it('should add numbers to total', function(){
         //TODOS: Expectations
         const calculator=new Calculator();
         calculator.add(5);
@@ -27,8 +27,27 @@ xdescribe("calculator.js", ()=>{
         //TODOS: Expectations
         const calculator=new Calculator();
         calculator.total=25;
-        // expect total to be -5;
+        // expect total to be 5;
         calculator.divide(5);
         expect(calculator.total).toBe(5);
+        expect(calculator.total).toBeTruthy();
     });  
+
+    it('should initialize total', function(){
+        //TODOS: Expectations
+        const calculator=new Calculator();
+        
+        // expect total to be 0;
+        expect(calculator.total).toBe(0);
+        expect(calculator.total).toBeFalsy();
+    });
+
+    it('can be instantiated', function(){
+        //TODOS: Expectations
+        const calculator=new Calculator();
+        const calculator2=new Calculator();
+        // expect total to be truthy;
+        expect(calculator).toBeTruthy();
+        expect(calculator2).toBeTruthy();
+    });
 });
