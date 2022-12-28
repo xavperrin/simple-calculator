a047fa8da71dcf6089efc4724cca887cba4b727d
xavperrin <xl.perrin@gmail.com>
2022-12-28 18:19:31 +0100

    move convert.js

---
+++ convert.js
@@ -0,0 +1,13 @@
+function decimalToHexadecimal(decimal) {
+  let hexadecimal = "";
+  while (decimal > 0 || hexadecimal === "") {
+    let remainder = decimal % 16;
+    if (remainder < 10) {
+      hexadecimal = remainder + hexadecimal;
+    } else {
+      hexadecimal = String.fromCharCode(55 + remainder) + hexadecimal;
+    }
+    decimal = Math.floor(decimal / 16);
+  }
+  return hexadecimal;
+}
