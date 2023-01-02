7f5946c26727476de8a67f9c2a5fb5dbf562e244
xavperrin <xl.perrin@gmail.com>
2023-01-02 19:41:44 +0100

    redact labels for specs for testing main.js

---
+++ main.spec.js
@@ -0,0 +1,16 @@
+describe('main.js', ()=>{
+   describe('calculate()', ()=>{
+      xit('validate expressions');
+      xit('call add');
+      xit('call subtract');
+      xit('call multiply');
+      xit('call divide');
+      xit('validates operations');
+      xit('calls updateresult');
+   });
+   describe('updateresult()', ()=>{
+      xit('add result to DOM');
+     
+
+   });
+});
